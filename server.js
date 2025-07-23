b224b300c193ff6f6214b254172052f7a0e3b160
BLOODSHED MUNYARADZI CHIONDEGWA <chi24042@byui.edu>
2025-07-23 18:47:09 +0200

    new changes

--- server.js
+++ server.js
@@ -12,17 +12,25 @@ app
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     next();
-  })
-  .use('/', require('./routes'))
-  .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+  });
 
+// Redirect root to Swagger
+app.get('/', (req, res) => {
+  res.redirect('/api-docs');
+});
+
+// Swagger docs
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// API routes (adjust your route files accordingly)
+app.use('/api', require('./routes'));
 
 db.initDb((err) => {
   if (err) {
     console.log(err);
   } else {
     app.listen(port, () => {
-      console.log(`DB Connected and server running on port ${port}`);
+      console.log(`✅ DB Connected and server running on port ${port}`);
     });
   }
 });
